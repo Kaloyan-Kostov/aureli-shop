abc5f73f0f61ff0cd8ee9988ff4b6ca06c118e24
KaloyanKostov <kostov.kaloyan44@gmail.com>
2024-11-10 14:50:00 +0200

    for redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/hero";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
